afa087a5ee1ad637558c43e9820963c2a9ba47f8
Eli Kleinworm <elik12996@gmail.com>
2017-12-26 18:09:49 -0500

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * Project2 
+ * Stack and Queues
+ * Cans of Beans
+ * Node class
+ * 
+ * @author Elikleinworm
+ *
+ */
+public class Node 
+{
+		protected Beans data;
+		protected Node next;
+		public Node(Beans d){
+		data =d;
+		next=null;
+		
+	}
+}
